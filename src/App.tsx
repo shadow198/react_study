a2af933b171e84b36a0970759ae96b70fa1f57f4
shadow <wangys198@gmail.com>
2021-09-16 17:04:45 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" 
+          style={{width: '100px', height: "100px"}} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
